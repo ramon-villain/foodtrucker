f07880dc870c407494f5d902e90aaa3dc2f7581c
Ramon Villain <ramon.villain@gmail.com>
2014-07-21 02:29:43 -0300

    Requests lastSpots to dashboard

--- app/controllers/Admin/AdminController.php
+++ app/controllers/Admin/AdminController.php
@@ -1,10 +1,29 @@
 <?php
 
+use Foodtrucker\Spots\SpotRepository;
+use Foodtrucker\Tags\TagRepository;
+
 class Admin_AdminController extends BaseController {
+
+	private $spotRepository;
+	/**
+	 * @var TagRepository
+	 */
+	private $tagRepository;
+
+	/**
+	 * @param SpotRepository $spotRepository
+	 * @param TagRepository $tagRepository
+	 */
+	function __construct( SpotRepository $spotRepository, TagRepository $tagRepository) {
+		$this->spotRepository = $spotRepository;
+		$this->tagRepository = $tagRepository;
+	}
 
 	public function dashboard()
 	{
-		return View::make('back.pages.dashboard');
+		$data['spots'] = $this->spotRepository->getSpots();
+		return View::make('back.pages.dashboard', compact('data'));
 	}
 
 }
