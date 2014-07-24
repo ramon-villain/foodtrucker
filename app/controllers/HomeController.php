aaa3187fd8693b6b1a1f21afca9f2f5f1bc3530c
Ramon Villain <ramon.villain@gmail.com>
2014-07-24 02:49:35 -0300

    Configurando get & loop banners na home(front)

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,24 +1,31 @@
 <?php
 
+use Foodtrucker\BannersHome\BannerRepository;
 use Foodtrucker\Configs\ConfigRepository;
 use Foodtrucker\Configs\SetFeatured\SetFeaturedCommand;
 
 class HomeController extends BaseController {
 
 	protected $configRepository;
+	/**
+	 * @var BannerRepository
+	 */
+	private $bannerRepository;
 
-	function __construct( ConfigRepository $configRepository ) {
+	function __construct( ConfigRepository $configRepository, BannerRepository $bannerRepository) {
 		$this->configRepository = $configRepository;
+		$this->bannerRepository = $bannerRepository;
 	}
 
 	public function index()
 	{
 		$featured = $this->configRepository->getFeatured();
+		$banners = $this->bannerRepository->getBanners(5);
 		if($featured == null){
 			$featured = new SetFeaturedCommand('https://res.cloudinary.com/enjoei/image/upload/c_fill,h_330,w_276/avvdeqvbnj0omkxnvuub','https://res.cloudinary.com/enjoei/image/upload/c_fill,h_330,w_276/avvdeqvbnj0omkxnvuub        ','Food Trucker','Em Destaque');
 			Session::forget('modal');
 		}
-		return View::make('front.pages.home', compact('featured'));
+		return View::make('front.pages.home', compact('featured', 'banners'));
 	}
 
 }
--- app/views/front/pages/home.blade.php
+++ app/views/front/pages/home.blade.php
@@ -2,22 +2,26 @@
 
 @section('content')
 <div id="main" class="col-13">
-	<h1>a</h1>
+	<div id="mapa" class="mb20" style="background: #000;height: 446px;display: block;width: 100%;"></div>
 	<div id="truckDaSemana" class="col-5 alpha widget">
 		<div class="title blue"><i class="fa fa-trophy"></i><h2>{{$featured->title}}</h2></div>
-		<div class="body">
-			<img src="{{$featured->image}}" alt=""/>
+		<div class="body" style="background-image: url({{$featured->image}})">
 			<h3>{{$featured->body}}</h3>
 		</div>
 	</div>
-	<div id="anota-ai" class="col-8 omega widget">
-		<div class="title green"><i class="fa fa-calendar-o"></i><h2>Anota Ai!</h2> <span>Hoje</span></div>
-		<div class="body">
-			<ul>
+
+	<div id="anota-ai" class="col-8 omega widget ">
+		<div class="title green"><i class="fa fa-calendar-o"></i><h2>Anota Ai!</h2> <!--<span>Hoje</span>--></div>
+		<div class="body carousel">
+			<ul class="bjqs">
+				@foreach ($banners as $banner)
 				<li>
-					<img src="{{$featured->image}}" alt=""/>
-					<h3>{{$featured->body}}</h3>
+					<a href="{{$banner->url}}" target="_blank">
+						<img src="{{$banner->imagem}}" alt=""/>
+						<h3>{{$banner->body}}</h3>
+					</a>
 				</li>
+				@endforeach
 			</ul>
 		</div>
 
@@ -26,4 +30,21 @@
 <div id="sidebar" class="col-7">
 	@include('front._includes.next-spots')
 </div>
+@stop
+@section('scripts')
+{{HTML::script('js/bjqs-1.3.min.js')}}
+<script>
+	$('.carousel').bjqs({
+		'height' : 245,
+		'width' : 404,
+		'responsive' : false,
+		animtype : 'slide',
+		nexttext : '<i class="fa fa-chevron-right fa-lg"></i>',
+		prevtext : '<i class="fa fa-chevron-left fa-lg"></i>'
+	});
+</script>
+@stop
+
+@section('css')
+{{HTML::style('css/vendor/bjqs.css')}}
 @stop
