446365428b1708ae0a5f20789c22abe4667745a1
Ramon Villain <ramon.villain@gmail.com>
2014-07-24 14:36:28 -0300

    Igniscode Local

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('igniscode-1.home'),
+	'local' => array('IGNISCODE-1.local','igniscode-1.home'),
 
 ));
 
