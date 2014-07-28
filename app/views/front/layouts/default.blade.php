2cc05e440362dbda5095e86fe9eef610f993fa26
Ramon Villain <ramon.villain@gmail.com>
2014-07-28 18:46:12 -0300

    Adicionando Footer

--- app/views/front/layouts/default.blade.php
+++ app/views/front/layouts/default.blade.php
@@ -14,13 +14,34 @@
 		<header>
 			<div id="logo" class="col-3"><a href="/"><img src="/images/foodtrucker-t.svg" alt="" height="100"/></a></div>
 			<div id="menu" class="col-12">@include('front._includes.menu')</div>
-			<div class="col-5">@include('front._includes.busca')</div>
+			<div class="col-5">@include('front._includes.busca',['formClass' => ''])</div>
 		</header>
 		<div id="content">
 			@yield('content')
 		</div>
 	</div>
 </div>
+<footer>
+	<div class="page">
+		<div class="container">
+			<div class="col-5 copyright">
+				<img src="/images/foodtrucker-bye.png" alt=""/>
+				<p>2014 © {{link_to_route('home', 'FOODTRUCKER.COM.br')}}<br/>
+				TODOS OS DIREITOS RESERVADOS</p>
+			</div>
+			<div class="col-3 sitemap">
+				<ul>
+					<h3>MAPA DO SITE</h3>
+					<li>{{link_to_route('home', '- HOME')}}</li>
+					<li>{{link_to_route('home', '- SOBRE NÓS')}}</li>
+					<li>{{link_to_route('home', '- TRUCKS')}}</li>
+					<li>{{link_to_route('home', '- BLOG')}}</li>
+					<li>{{link_to_route('home', '- CONTATO')}}</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
 {{HTML::script('js/jquery.min.js')}}
 {{HTML::script('js/jquery.cookie.min.js')}}
 {{HTML::script('js/main.js')}}
