d73f325d820fc8764ab131bc97c7346b782c2d64
Ramon Villain <ramon.villain@gmail.com>
2014-07-29 14:39:34 -0300

    Trucks Admin Menu

--- app/views/back/_includes/menu.blade.php
+++ app/views/back/_includes/menu.blade.php
@@ -1,6 +1,7 @@
 <ul>
 	<li>{{link_to_route('dashboard', 'Home' )}}</li>
 	<li><a href="">Sobre Nós</a></li>
+	<li>{{link_to_route('truck_admin_path', 'Trucks')}}</li>
 	<li>{{link_to_route('spot_admin_path', 'Spots')}}</li>
 	<li>{{link_to_route('tag_admin_path', 'Tags')}}</li>
 	<li>{{link_to_route('config_admin_path', 'Configurações')}}</li>
