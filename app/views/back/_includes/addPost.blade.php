082a7507d6f9cf980bd3635e57ad24b092881cc1
Ramon Villain <ramon.villain@gmail.com>
2014-07-30 20:19:59 -0300

    Include AddPost (blog)

--- app/views/back/_includes/addPost.blade.php
+++ app/views/back/_includes/addPost.blade.php
@@ -5,17 +5,17 @@
 	<div class="body bordered">
 		{{Form::open(['route' => 'new_post_admin_path', 'class' => 'form', 'files' => true])}}
 
-		{{Form::label('title', 'Título:')}}
-		<div class="wrapInput">{{Form::text('title','',['placeholder' => 'Postagem marota'])}}</div>
+		{{Form::label('titulo', 'Título:')}}
+		<div class="wrapInput">{{Form::text('titulo','',['placeholder' => 'Postagem marota'])}}</div>
 
 		{{Form::label('body', 'Conteúdo:')}}
 		<div class="wrapInput wrapTextarea">{{Form::textarea('body','',['placeholder' => 'Conteúdo legal'])}}</div>
 
-		{{Form::label('image', 'Imagem em Publicação:')}}
-		<div class="wrapInput">{{Form::file('image')}}</div>
+		{{Form::label('imagem', 'Imagem em Publicação:')}}
+		<div class="wrapInput">{{Form::file('imagem')}}</div>
 
-		{{Form::label('body', 'Data de Publicação:')}}
-		<div class="wrapInput wrapTextarea">{{Form::textarea('body','',['placeholder' => 'Conteúdo legal'])}}</div>
+		{{Form::label('publish_at', 'Data de Publicação:')}}
+		<div class="wrapInput">{{Form::text('publish_at','',['placeholder' => '29/06/2014'])}}</div>
 
 		{{Form::submit('Adicionar Post', ['class' => 'btn btn-green fr'] )}}
 		{{Form::close()}}
