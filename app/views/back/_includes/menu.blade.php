33bdf44af575d31b92d5c54b049a7fd7fd82d559
Ramon Villain <ramon.villain@gmail.com>
2014-07-30 17:22:57 -0300

    Blog Menu

--- app/views/back/_includes/menu.blade.php
+++ app/views/back/_includes/menu.blade.php
@@ -5,6 +5,6 @@
 	<li>{{link_to_route('spot_admin_path', 'Spots')}}</li>
 	<li>{{link_to_route('tag_admin_path', 'Tags')}}</li>
 	<li>{{link_to_route('config_admin_path', 'Configurações')}}</li>
-	<li><a href="">Blog</a></li>
+	<li>{{link_to_route('blog_admin_path', 'Blog')}}</li>
 	<li>{{link_to_route('logout_path', 'Logout')}}</li>
 </ul>
