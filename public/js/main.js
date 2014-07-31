cd804421bf643c18efa792e6f154caacf9bc15ee
Ramon Villain <ramon.villain@gmail.com>
2014-07-31 19:59:38 -0300

    Active page on menu

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,8 @@
 $(function() {
     var currentUrl = $('#current_url').val();
     $("#menu ul li a[href='" + currentUrl + "']").addClass('active');
-
+    $('.alert .close').click(function(){
+        console.log('a');
+        $(this).parent('li').parent('ul').parent('.alert').fadeOut(350);
+    });
 });
