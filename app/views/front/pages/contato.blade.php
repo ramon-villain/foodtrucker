f41d6cd6bf5c60814fc90040c774ef74d522852b
Ramon Villain <ramon.villain@gmail.com>
2014-07-31 20:14:22 -0300

    Página de contato

---
+++ app/views/front/pages/contato.blade.php
@@ -0,0 +1,30 @@
+@extends('front.layouts.default')
+@section('content')
+<div id="main" class="col-13">
+	<div class="pageTitle">
+		<h1>Fale Conosco</h1>
+	</div>
+	@include('front._includes.errors_alert')
+	{{ Form::open(['route' => 'contato_path', 'class' => 'form']) }}
+		{{Form::label('nome', 'Seu Nome:*')}}
+		<div class="wrapInput">{{Form::text('nome','',['placeholder' => 'Antônio Amigo da Silva'])}}</div>
+		{{Form::label('email_contato', 'Seu Email:*')}}
+		<div class="wrapInput">{{Form::email('email_contato','',['placeholder' => 'antonio@exemplo.com', 'required' => 'required'])}}</div>
+		{{Form::label('telefone', 'Seu Telefone:')}}
+		<div class="wrapInput">{{Form::text('telefone','',['placeholder' => '(11) 3344-4433'])}}</div>
+		{{Form::label('mensagem', 'Sua Mensagem:*')}}
+		<div class="wrapInput wrapTextarea">{{Form::textarea('mensagem','',['placeholder' => 'Olá, sou Antônio Amigo da Silva…'])}}</div>
+		{{Form::submit('Enviar', ['class' => 'btn btn-green'])}}
+	{{ Form::close() }}
+</div>
+<div id="sidebar" class="col-7">
+	@include('front._includes.next-spots')
+	@include('front._includes.newsletter')
+</div>
+@stop
+@section('scripts')
+{{HTML::script('js/jquery.mask.min.js')}}
+<script>
+	$('#telefone').mask('(00) 00009-0000');
+</script>
+@stop
