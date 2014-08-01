25fc0f52836c0afc03ea7005182ec2a1640a7b40
Ramon Villain <ramon.villain@gmail.com>
2014-08-01 16:58:47 -0300

    Limpando scripts desnecessários

--- app/views/back/pages/truck_edit.blade.php
+++ app/views/back/pages/truck_edit.blade.php
@@ -51,29 +51,3 @@
 </div>
 
 @stop
-
-@section('scripts')
-{{HTML::script('js/wysihtml5-0.3.0.js')}}
-{{HTML::script('js/bootstrap.min.js')}}
-{{HTML::script('js/bootstrap-wysihtml5.js')}}
-{{HTML::script('js/jquery.datetimepicker.js')}}
-<script>
-	$('.editor').wysihtml5({
-		"font-styles": false,
-		"stylesheets": false
-	});
-	$('#publish_at').appendDtpicker({
-		"locale": "br",
-		"autodateOnStart": false,
-		"calendarMouseScroll": false,
-		"dateFormat":'YYYY-MM-DD hh:mm:00'
-	});
-
-</script>
-@stop
-
-@section('css')
-{{HTML::style('css/vendor/jquery.datetimepicker.css')}}
-{{HTML::style('css/vendor/bootstrap.min.css')}}
-{{HTML::style('css/vendor/bootstrap-wysihtml5.css')}}
-@stop
