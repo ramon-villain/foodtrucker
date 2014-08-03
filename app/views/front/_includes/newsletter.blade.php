bbd622a3e0b40ad940ebb16ec210d021b37d9109
Ramon Villain <ramon.villain@gmail.com>
2014-08-03 12:07:11 -0300

    [VIEW + CSS] Ajustando tamanho envelope Newsletter Form

--- app/views/front/_includes/newsletter.blade.php
+++ app/views/front/_includes/newsletter.blade.php
@@ -1,5 +1,5 @@
 {{ Form::open(['route' => 'newsletter_path', 'class' => 'form red widget', 'id' => 'newsletter']) }}
-	<h2><i class="fa fa-envelope-o"></i>Assine nossa newsletter</h2>
+	<h2><i class="fa fa-envelope-o" style="font-size: 18px;position:relative;top: -1px;"></i>Assine nossa newsletter</h2>
 	<div class="wrapInput">{{ Form::email('email', '', ['placeholder' => 'email@exemplo.com', 'required' => 'required']) }}</div>
 	{{ Form::submit('OK', ['class' => 'text-btn']) }}
 {{ Form::close() }}
