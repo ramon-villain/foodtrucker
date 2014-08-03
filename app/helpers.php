ae1b305464387e14fd3d99b6719fe92324e1ab18
Ramon Villain <ramon.villain@gmail.com>
2014-08-03 00:03:30 -0300

    [BACK] Novo Método para datas dos Eventos

--- app/helpers.php
+++ app/helpers.php
@@ -4,3 +4,9 @@ function dataSpotFront($dia, $hora){
 	$final = new DateTime($dia.' '. $hora);
 	return $final->format('d/m - h:i');
 }
+function dataEvento($data){
+	$data = explode('-', $data);
+	$mes = $data[1];
+	$dia = $data[2];
+	return "<span class='dia'>$dia</span><span class='mes'>$mes</span>";
+}
