c59c9a66052ccf36a4eb58dd65ea87ba0a3ac677
Ramon Villain <ramon.villain@gmail.com>
2014-08-03 14:30:45 -0300

    [CTRL] Fix Edit Page

--- app/controllers/Admin/BlogController.php
+++ app/controllers/Admin/BlogController.php
@@ -37,7 +37,6 @@ class Admin_BlogController extends BaseController {
 
 
 	public function edit($id){
-		$this->newPostForm->validate(Input::all());
 		$data['post'] = $this->blogRepository->getPostById($id);
 		$data['title'] = 'Editando '. $data['post']->titulo;
 		return View::make('back.pages.post_edit', compact('data'));
