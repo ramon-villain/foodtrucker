e5d6cc53e92e5afa460dd1aad051e32bb37c02df
Ramon Villain <ramon.villain@gmail.com>
2014-08-04 19:52:02 -0300

    [REPO] Atualizando métodos de atualização e inserção de Trucks

--- app/Foodtrucker/Trucks/TruckRepository.php
+++ app/Foodtrucker/Trucks/TruckRepository.php
@@ -32,7 +32,7 @@ class TruckRepository {
 		return Truck::where('id', $id)->first();
 	}
 
-	public function updateTruck($id, $nome, $logo, $description,$pagamento, $facebook, $instagram, $maisPedido, $extras) {
+	public function updatesTruck($id, $nome, $logo, $description,$pagamento, $facebook, $instagram, $maisPedido, $extras) {
 		return Truck::where('id', $id)->update([
 			'nome' => $nome,
 			'logo' => $logo,
@@ -47,6 +47,54 @@ class TruckRepository {
 
 	public function getActualImage( $id ) {
 		return Truck::where('id', $id)->pluck('logo');
+	}
+
+	public function getTruckBySlug( $slug ) {
+		return Truck::where('slug', $slug)->first();
+	}
+
+	public function getCategorias() {
+		return \DB::table('categorias')->orderBy('id','asc')->lists('nome');
+	}
+	public function getCategoriaNameById($id){
+		return \DB::table('categorias')->where('id',$id)->pluck('nome');
+	}
+
+	public function saveNewTruck( $nome, $slug, $logo, $especialidade, $cat_id, $mais_pedido, $site, $facebook, $instagram, $preco, $pagamento, $description, $imagens, $extras ) {
+		return Truck::insert([
+			'nome' => $nome,
+			'slug' => $slug,
+			'logo' => $logo,
+			'especialidade' => $especialidade,
+			'cat_id' => $cat_id,
+			'mais_pedido' => $mais_pedido,
+			'site' => $site,
+			'facebook' => $facebook,
+			'instagram' => $instagram,
+			'preco' => $preco,
+			'pagamento' => $pagamento,
+			'description' => $description,
+			'imagens' => $imagens,
+			'extras' => $extras
+		]);
+	}
+
+	public function updateTruck( $id, $nome, $logo, $especialidade, $cat_id, $mais_pedido, $site, $facebook, $instagram, $preco, $pagamento, $description, $imagens, $extras ) {
+		return Truck::where('id', $id)->update([
+			'nome' => $nome,
+			'logo' => $logo,
+			'especialidade' => $especialidade,
+			'cat_id' => $cat_id,
+			'mais_pedido' => $mais_pedido,
+			'site' => $site,
+			'facebook' => $facebook,
+			'instagram' => $instagram,
+			'preco' => $preco,
+			'pagamento' => $pagamento,
+			'description' => $description,
+			'imagens' => $imagens,
+			'extras' => $extras
+		]);
 	}
 
 } 
