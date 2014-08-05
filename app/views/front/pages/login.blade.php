064fc5f49fa380d40962bb9619b2bb363b8fa1bd
Ramon Villain <ramon.villain@gmail.com>
2014-08-05 11:28:37 -0300

    [VIEW] Fix Margem inferior

--- app/views/front/pages/login.blade.php
+++ app/views/front/pages/login.blade.php
@@ -4,16 +4,18 @@
 
 @include('front._includes.errors')
 <div id="main" class="col-13">
-{{Form::open(['route' => 'login_path','class' => 'form col-8'])}}
-	<h1>Faça seu login, aqui. :)</h1>
-	{{Form::label('email', 'Email')}}
-	<div class="wrapInput">{{Form::email('email','', array('placeholder' => 'email@exemplo.com', 'required' => 'required'))}}</div>
+	<div class="widget" style="clear:both;overflow: hidden">
+		{{Form::open(['route' => 'login_path','class' => 'form col-8'])}}
+		<h1>Faça seu login, aqui. :)</h1>
+		{{Form::label('email', 'Email')}}
+		<div class="wrapInput">{{Form::email('email','', array('placeholder' => 'email@exemplo.com', 'required' => 'required'))}}</div>
 
-	{{Form::label('password', 'Senha')}}
-	<div class="wrapInput">{{Form::password('password','', array('required' => 'required'))}}</div>
+		{{Form::label('password', 'Senha')}}
+		<div class="wrapInput">{{Form::password('password','', array('required' => 'required'))}}</div>
 
-	{{Form::submit('Logar', ['class' => 'btn btn-green fr'])}}
+		{{Form::submit('Logar', ['class' => 'btn btn-green fr'])}}
 
-{{Form::close()}}
+		{{Form::close()}}
 	</div>
+</div>
 @stop
