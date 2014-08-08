618f5f200cddd2b323f12f6bb779e2803e5aeb8b
Ramon Villain <ramon.villain@gmail.com>
2014-08-08 14:47:46 -0300

    [CTRL] O milagroso método spotJs

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -4,7 +4,11 @@ use Foodtrucker\BannersHome\BannerRepository;
 use Foodtrucker\Configs\ConfigRepository;
 use Foodtrucker\Configs\SetFeatured\SetFeaturedCommand;
 use Foodtrucker\Forms\NewsletterForm;
+use Foodtrucker\Spots\Spot;
 use Foodtrucker\Spots\SpotRepository;
+use Foodtrucker\Tags\Tag;
+use Foodtrucker\Tags\TagTruck;
+use Foodtrucker\Trucks\Truck;
 use Foodtrucker\Trucks\TruckRepository;
 
 class HomeController extends BaseController {
@@ -37,7 +41,10 @@ class HomeController extends BaseController {
 
 	public function index()
 	{
+		$categorias = $this->truckRepository->getCategorias();
+		$trucks = $this->truckRepository->getTrucks();
 		$spots = $this->spotRepository->getSpotsActive();
+		$spotsFuturos = $this->spotRepository->getFutureSpotsActive();
 		$featured = $this->configRepository->getFeatured();
 		$banners = $this->bannerRepository->getBanners(5);
 		$last_truck = $this->truckRepository->getTruckById('last');
@@ -46,7 +53,7 @@ class HomeController extends BaseController {
 			$featured = new SetFeaturedCommand('https://res.cloudinary.com/enjoei/image/upload/c_fill,h_330,w_276/avvdeqvbnj0omkxnvuub','https://res.cloudinary.com/enjoei/image/upload/c_fill,h_330,w_276/avvdeqvbnj0omkxnvuub        ','Food Trucker','Em Destaque');
 			Session::forget('modal');
 		}
-		return View::make('front.pages.home', compact('featured', 'banners', 'spots', 'last_truck', 'imagens'));
+		return View::make('front.pages.home', compact('featured', 'banners', 'spots', 'last_truck', 'imagens', 'categorias', 'trucks', 'spotsFuturos'));
 	}
 
 	public function newsletter(){
@@ -57,6 +64,35 @@ class HomeController extends BaseController {
 			'updated_at'    => new DateTime
 		]);
 		return Redirect::back()->with('message','Email Cadastrado com Sucesso!');
+	}
+
+	public function spotsJs(){
+			$spotsAtivo = Spot::where('active', 1)->where('abertura', '=', new DateTime('today'))->get()->toArray();
+			for($i=0; $i < count($spotsAtivo); $i++){
+				$long = DB::table('spot_details')->where('spot_id',$spotsAtivo[$i]['id'])->pluck('long');
+				$lat = DB::table('spot_details')->where('spot_id',$spotsAtivo[$i]['id'])->pluck('lat');
+				$truck = Truck::where('id', $spotsAtivo[$i]['truck_id'])->first()->toArray();
+				$tags_id = TagTruck::where('truck_id',$truck['id'])->where('spot_id', null)->lists('tag_id');
+				$tags_spot = TagTruck::where('truck_id',$truck['id'])->where('spot_id', $spotsAtivo[$i]['id'])->lists('tag_id');
+				$tags = [];
+				for($s=0;$s < count($tags_id) ; $s++){
+					$tags[] = Tag::where('id', $tags_id[$s])->pluck('tag');
+				}
+				for($r=0;$r < count($tags_spot); $r++){
+					$t = Tag::where('id', $tags_spot[$r])->pluck('tag');
+					if(!in_array($t,$tags)){
+						$tags[] = $t;
+					}
+				}
+				$servico = unserialize($truck['pagamento']);
+				$serv = [];
+				for($x=0; $x < count($servico); $x++){
+					$serv[] = respostaServico($servico[$x]);
+				}
+				$spotsAtivo[$i]['dataParsed'] = dataMapFront($spotsAtivo[$i]['abertura'], $spotsAtivo[$i]['inicio'],$spotsAtivo[$i]['encerramento'], $spotsAtivo[$i]['fim']);
+				array_push($spotsAtivo[$i], [$long, $lat], $truck, $serv, $tags);
+			}
+			return Response::json($spotsAtivo);
 	}
 
 
